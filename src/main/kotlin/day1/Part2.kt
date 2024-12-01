11e55dbe247bf8cde69cb352db61b45bee15d361
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-01 11:02:26 +0000

    Day 1 Part 2

--- src/main/kotlin/day1/Part2.kt
+++ src/main/kotlin/day1/Part2.kt
@@ -7,6 +7,14 @@ fun main() {
 
 object Part2 {
     fun calc(input: List<String>): Int {
-        TODO()
+        val pairs = load(input)
+        val first = pairs.map { it.first }
+        val second = pairs.map { it.second }
+
+        return first.sumOf { f -> second.count { s -> f == s } * f }
+    }
+
+    private fun load(input: List<String>): List<Pair<Int, Int>> {
+        return input.map { it.split("   ") }.map { it[0].toInt() to it[1].toInt() }
     }
 }
