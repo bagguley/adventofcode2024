22840ebeb5e70312dd183d8577574f2cff887788
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-01 11:02:17 +0000

    Day 1 Part 1

--- src/main/kotlin/day1/Part1.kt
+++ src/main/kotlin/day1/Part1.kt
@@ -1,5 +1,7 @@
 package day1
 
+import kotlin.math.abs
+
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
@@ -7,6 +9,14 @@ fun main() {
 
 object Part1 {
     fun calc(input: List<String>): Int {
-        TODO()
+        val pairs = load(input)
+        val first = pairs.map { it.first }.sorted()
+        val second = pairs.map { it.second }.sorted()
+
+        return first.zip(second) { a, b -> abs(a - b) }.sum()
+    }
+
+    private fun load(input: List<String>): List<Pair<Int, Int>> {
+        return input.map { it.split("   ") }.map { it[0].toInt() to it[1].toInt() }
     }
 }
