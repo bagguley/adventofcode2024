b576fd335958170e960f02e9f83325d226c664de
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-03 15:06:00 +0000

    Day 1 Part 1a

--- src/main/kotlin/day1/Part1.kt
+++ src/main/kotlin/day1/Part1.kt
@@ -5,6 +5,7 @@ import kotlin.math.abs
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+    println(Part1a.calc(data))
 }
 
 object Part1 {
@@ -19,4 +20,10 @@ object Part1 {
     private fun load(input: List<String>): List<Pair<Int, Int>> {
         return input.map { it.split("   ") }.map { it[0].toInt() to it[1].toInt() }
     }
+}
+
+object Part1a {
+    fun calc(input: List<String>): Int = input.map{it.split("   ")}
+        .map{it[0].toInt() to it[1].toInt()}.let{it.map{it.first}.sorted()
+            .zip(it.map{it.second}.sorted()){a,b->abs(a-b)}.sum()}
 }
