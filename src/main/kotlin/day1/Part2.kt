1fcfdb08c88ee5d74c13c50e8825e412a149608d
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-03 15:06:09 +0000

    Day 1 Part 2a

--- src/main/kotlin/day1/Part2.kt
+++ src/main/kotlin/day1/Part2.kt
@@ -3,6 +3,7 @@ package day1
 fun main() {
     println(Part2.calc(testData))
     println(Part2.calc(data))
+    println(Part2a.calc(data))
 }
 
 object Part2 {
@@ -17,4 +18,10 @@ object Part2 {
     private fun load(input: List<String>): List<Pair<Int, Int>> {
         return input.map { it.split("   ") }.map { it[0].toInt() to it[1].toInt() }
     }
+}
+
+object Part2a {
+    fun calc(input: List<String>): Int = input.map{it.split("   ")}
+        .map{it[0].toInt() to it[1].toInt()}
+        .let{it.map{it.first}.sumOf{f->it.map{it.second}.count{s->f==s}*f}}
 }
