2846ae124441f365254b91e1883877fe3e2c0d9e
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-03 16:11:16 +0000

    Day 3 Part 1b

--- src/main/kotlin/day3/Part1.kt
+++ src/main/kotlin/day3/Part1.kt
@@ -4,6 +4,7 @@ fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
     println(Part1a.calc(data))
+    println(Part1b.calc(data))
 }
 
 object Part1 {
@@ -18,4 +19,9 @@ object Part1 {
 object Part1a {
     fun calc(input: String): Int = """mul\(\d+,\d+\)""".toRegex().findAll(input)
         .sumOf{it.value.drop(4).dropLast(1).split(",").let{it[0].toInt()*it[1].toInt()}}
+}
+
+object Part1b {
+    fun calc(input: String): Int = """mul\((\d+),(\d+)\)""".toRegex().findAll(input)
+        .sumOf{it.groupValues[1].toInt()*it.groupValues[2].toInt()}
 }
