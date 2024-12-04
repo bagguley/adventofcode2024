6702f17b177e635e17afe8fb4cba2eb1ad075b5c
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-04 08:04:39 +0000

    Day 4 Part 2

---
+++ src/main/kotlin/day4/Part2.kt
@@ -0,0 +1,25 @@
+package day4
+
+fun main() {
+    println(Part2.calc(testData))
+    println(Part2.calc(data))
+}
+
+object Part2 {
+    fun calc(input:List<String>): Int = input.find('A').count { input.valid("MAS", it) }
+
+    private fun List<String>.find(character: Char): List<Pair<Int, Int>> =
+        flatMapIndexed { y, s -> s.mapIndexed { x, c -> x to c }
+            .filter { it.second == character }.map { it.first to y } }
+
+    private fun List<String>.valid(word: String, point: Pair<Int,Int>): Boolean {
+        val w1 = extract(point, listOf(-1 to -1, 0 to 0, 1 to 1))
+        val w2 = extract(point, listOf(1 to -1, 0 to 0, -1 to 1))
+
+        return (w1 == word || w1 == word.reversed()) && (w2 == word || w2 == word.reversed())
+    }
+
+    private fun List<String>.extract(point: Pair<Int,Int>, steps: List<Pair<Int, Int>>): String =
+        steps.map { s -> getOrNull(point.second + s.second)?.getOrNull(point.first + s.first) ?: "" }
+            .joinToString("")
+}
