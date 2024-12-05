34c894cd312186c93d82f66475c880e25cd6b513
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-05 18:16:09 +0000

    Day 5 Part 2a

--- src/main/kotlin/day5/Part2.kt
+++ src/main/kotlin/day5/Part2.kt
@@ -3,6 +3,7 @@ package day5
 fun main() {
     println(Part2.calc(testData))
     println(Part2.calc(data))
+    println(Part2a.calc(data))
 }
 
 object Part2 {
@@ -20,4 +21,15 @@ object Part2 {
     private fun isValid(rules: Map<String, List<String>>, update: List<String>): Boolean =
         update.map { s -> update.dropWhile { it != s } }.dropLast(1)
             .all { l -> l.drop(1).all { rules[l.first()]?.contains(it) == true } }
+}
+
+object Part2a {
+    fun calc(input: List<String>): Int =
+        (input[0].split("\n").groupBy({ it.split("|")[0] }) { it.substringAfter("|") } to
+         input[1].split("\n").map { it.split(",") }).let { p -> p.second.filter { !it.map {
+             s -> it.dropWhile { it != s } }.dropLast(1).all { l -> l.drop(1).all {
+                 p.first[l.first()]?.contains(it) == true } } }.map {
+                     it.sortedWith { a, b -> if (p.first[a]?.contains(b) == true) 1
+                     else if (p.first[b]?.contains(a) == true) -1 else 0 } }
+         }.sumOf { it[it.size/2].toInt() }
 }
