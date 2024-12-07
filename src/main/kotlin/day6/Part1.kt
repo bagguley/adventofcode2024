6158fc855034b5ac55de45860e46c215ea90eeb1
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-07 22:21:51 +0000

    Day 6 Part 1a

--- src/main/kotlin/day6/Part1.kt
+++ src/main/kotlin/day6/Part1.kt
@@ -3,6 +3,7 @@ package day6
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+    println(Part1a.calc(data))
 }
 
 object Part1 {
@@ -28,5 +29,20 @@ object Part1 {
         }
 
         return visited.size
+    }
+}
+
+object Part1a {
+    fun calc(input: List<String>): Int {
+        val position = input.flatMapIndexed { y, s -> s.mapIndexed { x, c -> x to c }.filter { it.second == '^' }.map { it.first to y } }.first()
+        return move(position, 0 to -1, input, setOf(position))
+    }
+
+    private tailrec fun move(position: Pair<Int,Int>, direction: Pair<Int,Int>, input: List<String>, visited: Set<Pair<Int,Int>>): Int {
+        val next = input.getOrNull(position.second + direction.second)?.getOrNull(position.first + direction.first) ?: 'X'
+        if (next == 'X') return visited.size
+        if (next != '#') return move(position.first + direction.first to position.second + direction.second, direction,
+            input, visited + (position.first + direction.first to position.second + direction.second))
+        return move(position, -direction.second to direction.first, input, visited)
     }
 }
