ceaf3d8910554c73ab31ac572b566e1209e96f48
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-08 21:31:29 +0000

    Day 7 Part 2a

--- src/main/kotlin/day7/Part2.kt
+++ src/main/kotlin/day7/Part2.kt
@@ -5,6 +5,8 @@ import java.math.BigInteger
 fun main() {
     println(Part2.calc(testData))
     println(Part2.calc(data))
+    println(Part2a.calc(data))
+    println(Part2b.calc(data))
 }
 
 object Part2 {
@@ -14,4 +16,21 @@ object Part2 {
     private fun allEquations(head: BigInteger, list: List<BigInteger>): List<BigInteger> = if (list.isEmpty()) listOf(head) else
         allEquations(head + list.first(), list.drop(1)) + allEquations(head * list.first(), list.drop(1)) +
         allEquations("$head${list.first()}".toBigInteger(), list.drop(1))
+}
+
+object Part2a {
+    fun calc(input: List<String>): BigInteger = input.map { it.substringBefore(":").toBigInteger() to it.substringAfter(": ").split(" ").map { it.toBigInteger() } }
+        .filter { e -> allEquations(e.first, e.second.first(), e.second.drop(1)) }.sumOf { it.first }
+
+    private fun allEquations(target: BigInteger, head: BigInteger, list: List<BigInteger>): Boolean = if (head > target) false else
+        if (list.isEmpty()) target == head else allEquations(target, head + list.first(), list.drop(1)) || allEquations(target, head * list.first(), list.drop(1)) ||
+                allEquations(target, "$head${list.first()}".toBigInteger(), list.drop(1))
+}
+
+object Part2b {
+    fun calc(input: List<String>): Long = input.map { it.substringBefore(":").toLong() to it.substringAfter(": ").split(" ").map { it.toLong() } }
+        .filter { e -> allEquations(e.first, e.second.first(), e.second.drop(1)) }.sumOf { it.first }
+
+    private fun allEquations(target: Long, head: Long, list: List<Long>): Boolean = if (list.isEmpty()) target == head else
+        if (head > target) false else allEquations(target, head + list.first(), list.drop(1)) || allEquations(target, head * list.first(), list.drop(1))
 }
