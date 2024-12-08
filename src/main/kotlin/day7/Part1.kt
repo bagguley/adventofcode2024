2bf707308f075692655acd879caca061fcbb793b
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-08 21:31:19 +0000

    Day 7 Part 1a

--- src/main/kotlin/day7/Part1.kt
+++ src/main/kotlin/day7/Part1.kt
@@ -3,6 +3,7 @@ package day7
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+    println(Part1a.calc(data))
 }
 
 object Part1 {
@@ -11,4 +12,12 @@ object Part1 {
 
     private fun allEquations(head: Long, list: List<Long>): List<Long> = if (list.isEmpty()) listOf(head) else
         allEquations(head + list.first(), list.drop(1)) + allEquations(head * list.first(), list.drop(1))
+}
+
+object Part1a {
+    fun calc(input: List<String>) = input.map { it.substringBefore(":").toLong() to it.substringAfter(": ").split(" ").map { it.toLong() } }
+        .filter { e -> allEquations(e.first, e.second.first(), e.second.drop(1)) }.sumOf { it.first }
+
+    private fun allEquations(target: Long, head: Long, list: List<Long>): Boolean = if (list.isEmpty()) target == head else
+        if (head > target) false else allEquations(target, head + list.first(), list.drop(1)) || allEquations(target, head * list.first(), list.drop(1))
 }
