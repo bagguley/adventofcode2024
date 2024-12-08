211e9b79a3b0b2829b28501d742d6500af0f4b77
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-08 22:05:09 +0000

    Day 8 Part 1a

--- src/main/kotlin/day8/Part1.kt
+++ src/main/kotlin/day8/Part1.kt
@@ -3,6 +3,7 @@ package day8
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+    println(Part1a.calc(data))
 }
 
 object Part1 {
@@ -32,5 +33,23 @@ object Part1 {
         val firstAntiNode = first.first + diff.first to first.second + diff.second
         val secondAntiNode = second.first - diff.first to second.second - diff.second
         return listOf(firstAntiNode, secondAntiNode)
+    }
+}
+
+object Part1a {
+    fun calc(input: List<String>): Int = input.flatMap{it.filter{it != '.'}.toSet()}.toSet().flatMap{
+        input.combinations(it)}.flatMap{anti(it)}.toSet().count{it.first in input.indices && it.second in 0..<input[0].length}
+
+    private fun List<String>.combinations(char: Char): List<Pair<Pair<Int, Int>, Pair<Int, Int>>> = this.flatMapIndexed {
+            y: Int, s: String -> s.mapIndexed{x: Int, c: Char -> x to c}.filter{it.second == char}.map{it.first to y}}
+        .let{combinations(it.first(), it.drop(1))}
+
+    private fun combinations(head: Pair<Int, Int>, tail: List<Pair<Int,Int>>): List<Pair<Pair<Int,Int>,Pair<Int,Int>>> =
+        tail.map{head to it} + if (tail.size == 1) emptyList() else combinations(tail.first(), tail.drop(1))
+
+    private fun anti(pair: Pair<Pair<Int,Int>,Pair<Int,Int>>): List<Pair<Int,Int>> {
+        val diff = pair.first.first - pair.second.first to pair.first.second - pair.second.second
+        return listOf(pair.first.first + diff.first to pair.first.second + diff.second,
+            pair.second.first - diff.first to pair.second.second - diff.second)
     }
 }
