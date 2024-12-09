080998070c7dcc52a27c32248b1fae73e0f690db
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-09 22:53:53 +0000

    Day 9 Part 1a

--- src/main/kotlin/day9/Part1.kt
+++ src/main/kotlin/day9/Part1.kt
@@ -5,6 +5,10 @@ import java.math.BigInteger
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+    println(Part1a.calc(testData))
+    println(Part1a.calc(data))
+    println(Part1b.calc(testData))
+    println(Part1b.calc(data))
 }
 
 object Part1 {
@@ -49,5 +53,25 @@ object Part1 {
             }
         }
         return sum
+    }
+}
+
+object Part1a {
+    fun calc(input: String): BigInteger {
+        val pairs = input.windowed(2, 2, true) { it[0].digitToInt() to (it.getOrNull(1)?.digitToInt() ?: 0) }
+        val (files,spaces) = pairs.unzip()
+        val rev = sequence { files.mapIndexed { i, c -> i to c }.reversed().forEach { p -> repeat(p.second) { yield(p.first) } } }.iterator()
+        val z = files.mapIndexed { i, c -> i to c }.zip(spaces).flatMap { p -> List(p.first.second) { p.first.first } + List(p.second) { rev.next() } }
+
+        return z.mapIndexed { i, c -> (i * c).toBigInteger() }.take(files.sum()).fold(BigInteger.ZERO){a,b->a+b}
+    }
+}
+
+object Part1b {
+    fun calc(input: String): BigInteger {
+        val (files,spaces) = input.windowed(2, 2, true) { it[0].digitToInt() to (it.getOrNull(1)?.digitToInt() ?: 0) }.unzip()
+        val rev = sequence { files.mapIndexed { i, c -> i to c }.reversed().forEach { p -> repeat(p.second) { yield(p.first) } } }.iterator()
+        return files.mapIndexed { i, c -> i to c }.zip(spaces).flatMap { p -> List(p.first.second) { p.first.first } + List(p.second) { rev.next() } }
+            .mapIndexed { i, c -> (i * c).toBigInteger() }.take(files.sum()).fold(BigInteger.ZERO){a,b->a+b}
     }
 }
