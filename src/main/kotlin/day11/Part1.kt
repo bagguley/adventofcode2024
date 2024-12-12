1814707b7773c2055fc798fed4bf37d53dcf1b07
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-12 16:53:37 +0000

    Day 11 Part 1a

--- src/main/kotlin/day11/Part1.kt
+++ src/main/kotlin/day11/Part1.kt
@@ -3,6 +3,7 @@ package day11
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+    println(Part1a.calc(data))
 }
 
 object Part1 {
@@ -19,4 +20,10 @@ object Part1 {
 
         return list.size
     }
+}
+
+object Part1a {
+    fun calc(input: String): Int =
+        (1..25).fold(input.split(" ")) { l, _ -> l.flatMap { if (it == "0") listOf("1") else if (it.length and 1 == 0)
+            listOf(it.drop(it.length/2), it.dropLast(it.length/2).toLong().toString()) else listOf((it.toLong()*2024).toString()) } }.size
 }
