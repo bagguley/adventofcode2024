8ab9ca680483bdebbb8c4bbf3e1908ef12710157
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-20 11:20:53 +0000

    Day 20 Part 2

---
+++ src/main/kotlin/day20/Part2.kt
@@ -0,0 +1,30 @@
+package day20
+
+import util.*
+
+fun main() {
+    println(Part2.calc(testData, 50))
+    println(Part2.calc(data, 100))
+}
+
+object Part2 {
+    fun calc(input: List<String>, saving: Int, cheatLength: Int = 20): Int {
+        val width = input[0].length
+        val height = input.size
+        val map = BoundedMap(width, height, '#')
+        val walls = input.findChar('#')
+        val start = input.findChar('S').first()
+        val end = input.findChar('E').first()
+
+        walls.forEach { map[it] = '#' }
+
+        val costFromEnd = map.findAllCost(end) { path -> Direction.ALL.map { Path(path.position + it, path.cost + 1) } }
+        val normalPath = map.findBestPath(start, end) { path -> Direction.ALL.map { Path(path.position + it, path.cost + 1) } }
+
+        val allPairs = normalPath.flatMap { p -> (normalPath - p).map { n -> p to n } }
+            .filter { it.first.distanceTo(it.second) <= cheatLength }
+            .filter { costFromEnd[it.first]!! > costFromEnd[it.second]!! }
+
+        return allPairs.filter { costFromEnd[it.first]!! - (it.first.distanceTo(it.second)) >= costFromEnd[it.second]!! + saving }.size
+    }
+}
