b8606b0acf6e0e68a21aeabdbfb43e46eb81f9b5
Wayne Bagguley <wayne.bagguley@gmail.com>
2024-12-23 21:42:31 +0000

    Day 23 Part 1

---
+++ src/main/kotlin/day23/Part1.kt
@@ -0,0 +1,47 @@
+package day23
+
+fun main() {
+    println(Part1.calc(testData))
+    println(Part1.calc(data))
+}
+
+object Part1 {
+    fun calc(input: List<String>): Int {
+        val lan = input.map { it.split("-") }
+
+        val map = mutableMapOf<String, MutableSet<String>>()
+
+        lan.forEach { (a, b) ->
+            if (map.containsKey(a)) {
+                map[a]!!.add(b)
+            } else {
+                map[a] = mutableSetOf(b)
+            }
+
+            if (map.containsKey(b)) {
+                map[b]!!.add(a)
+            } else {
+                map[b] = mutableSetOf(a)
+            }
+        }
+
+        var count = 0
+
+        val seen = mutableSetOf<Set<String>>()
+        for (e in map) {
+            val f = e.key
+            for (v in e.value) {
+                for (p in map[v]!!) {
+                    if (!seen.contains(setOf(f,v,p)) && (f.startsWith('t' ) || v.startsWith('t') || p.startsWith('t'))) {
+                        if (map[p]!!.contains(f)) {
+                            count++
+                            seen.add(setOf(f, v, p))
+                        }
+                    }
+                }
+            }
+        }
+
+        return count
+    }
+}
